4b2b2ba29ea025e9e12736603762993eb4dbe1e3
Nash Andrew <112688217+nashie1004@users.noreply.github.com>
2024-11-20 07:47:31 +0800

    Add project files.

---
+++ IdentityPractice/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+            
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpGet("/getTodos")]
+        public IActionResult GetTodos()
+        {
+            var list = new List<string>()
+            {
+                "Todo item 1"
+                ,"Todo item 2"
+            };
+
+            return Ok(list);
+        }
+    }
+}
